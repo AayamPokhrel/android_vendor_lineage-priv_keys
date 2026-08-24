fb8e278b8491e096f8944c4e7c1d973d42ddf470
Giovanni Ricca <giovanniricca@proton.me>
2026-08-24 12:45:16 +0200

    keys: Sync keys with Android 17
    Change-Id: Ib87f42551be17cf3f3bf7b7ea37dd9962e5d63ff

--- keys.mk
+++ keys.mk
@@ -34,6 +34,7 @@ PRODUCT_CERTIFICATE_OVERRIDES := \
     com.android.networkstack.tethering:com.android.networkstack.tethering.certificate.override \
     com.android.neuralnetworks:com.android.neuralnetworks.certificate.override \
     com.android.nfcservices:com.android.nfcservices.certificate.override \
+    com.android.npumanager:com.android.npumanager.certificate.override \
     com.android.ondevicepersonalization:com.android.ondevicepersonalization.certificate.override \
     com.android.os.statsd:com.android.os.statsd.certificate.override \
     com.android.permission:com.android.permission.certificate.override \
@@ -45,6 +46,8 @@ PRODUCT_CERTIFICATE_OVERRIDES := \
     com.android.scheduling:com.android.scheduling.certificate.override \
     com.android.sdkext:com.android.sdkext.certificate.override \
     com.android.support.apexer:com.android.support.apexer.certificate.override \
+    com.android.telecom:com.android.telecom.certificate.override \
+    com.android.telecomui:com.android.telecomui.certificate.override \
     com.android.telephony:com.android.telephony.certificate.override \
     com.android.telephonycore:com.android.telephonycore.certificate.override \
     com.android.telephonymodules:com.android.telephonymodules.certificate.override \
@@ -56,6 +59,7 @@ PRODUCT_CERTIFICATE_OVERRIDES := \
     com.android.virt:com.android.virt.certificate.override \
     com.android.vndk:com.android.vndk.certificate.override \
     com.android.vndk.current:com.android.vndk.current.certificate.override \
+    com.android.webapp:com.android.webapp.certificate.override \
     com.android.wifi:com.android.wifi.certificate.override \
     com.android.wifi.dialog:com.android.wifi.dialog.certificate.override \
     com.android.wifi.resources:com.android.wifi.resources.certificate.override \
@@ -72,6 +76,7 @@ PRODUCT_CERTIFICATE_OVERRIDES += \
     com.android.hardware.boot:com.android.hardware.certificate.override \
     com.android.hardware.cas:com.android.hardware.certificate.override \
     com.android.hardware.contexthub:com.android.hardware.certificate.override \
+    com.android.hardware.drm.clearkey:com.android.hardware.certificate.override \
     com.android.hardware.dumpstate:com.android.hardware.certificate.override \
     com.android.hardware.gatekeeper:com.android.hardware.certificate.override \
     com.android.hardware.gnss:com.android.hardware.certificate.override \
@@ -126,6 +131,9 @@ PRODUCT_CERTIFICATE_OVERRIDES += \
     ServiceConnectivityResources:com.android.connectivity.resources.certificate.override \
     ServiceUwbResources:com.android.uwb.resources.certificate.override \
     ServiceWifiResources:com.android.wifi.resources.certificate.override \
+    TelecomServiceResources:com.android.telecom.resources.certificate.override \
+    TelecomUi:com.android.telecomui.certificate.override \
+    WebAppService:com.android.webapp.certificate.override \
     WifiDialog:com.android.wifi.dialog.certificate.override
 
 PRODUCT_DEFAULT_DEV_CERTIFICATE := vendor/lineage-priv/keys/releasekey
